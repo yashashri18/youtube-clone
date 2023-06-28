b02bde6e6b9177b2a431c677c7d2576036c299a7
yashashri18 <57585968+yashashri18@users.noreply.github.com>
2023-06-28 23:20:29 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const BTN_ITEMS = ["All", "News", "Cooking", "Entertainment", "Sports", "Art", "Movies", "Songs","All", "News", "Cooking"]
 
-const  YOUTUBE_API_KEY = "AIzaSyCTYwJ5hQvgu59DAwYrHJfPvhVCIBQjeHo"
+const  YOUTUBE_API_KEY = ""
 
 export const YOUTUBE_FETCH_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key="+YOUTUBE_API_KEY 
 
